e6edd35bd8551e193342b96796f2473905c2b8a0
Swapnaneel <swapnaneel.log.in.16@gmail.com>
2026-04-15 18:20:30 -0400

    Creating the graph

--- agent/graph.py
+++ agent/graph.py
@@ -4,19 +4,35 @@ from dotenv import load_dotenv
 from pydantic import BaseModel , Field
 from prompts import *
 from states import *
+from langgraph.constants import END
+from langgraph.graph import StateGraph
 load_dotenv() 
 
-
-user_prompt = "Create a simple calculator web application"
-
-
-
-prompt = planner_prompt(user_prompt)
 
 
 
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
-resp = llm.with_structured_output(Plan).invoke(prompt)
 
-print(resp)
+def planner_agent(state:dict)->dict:
+    users_prompt = state["user_prompt"]
+    resp = llm.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
+    return {"plan":resp}
+
+
+
+
+
+
+
+graph = StateGraph(dict)
+
+graph.add_node("planner", planner_agent)
+graph.set_entry_point("planner")
+
+agent = graph.compile()
+
+user_prompt = "Create a simple calculator web application"
+result = agent.invoke({"user_prompt": user_prompt})
+
+print(result)
