744135bb38302142c134c8887ebb4385760adb7c
Swapnaneel <swapnaneel.log.in.16@gmail.com>
2026-04-15 20:35:46 -0400

    Architect Agent

--- agent/graph.py
+++ agent/graph.py
@@ -17,17 +17,28 @@ llm = ChatGroq(model="openai/gpt-oss-120b")
 def planner_agent(state:dict)->dict:
     users_prompt = state["user_prompt"]
     resp = llm.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
+
+    if resp is None:
+        raise ValueError("Planner agent failed to generate a plan.")
     return {"plan":resp}
 
 
-
-
+def architect_agent(state:dict)->dict:
+    plan: Plan = state["plan"]
+    resp =llm.with_structured_output(TaskPlan).invoke(architect_prompt(plan))
+    if resp is None:
+        raise ValueError("Architect agent failed to generate a task plan.")
+    
+    resp.plan = plan
+    return {"task_plan":resp}
 
 
 
 graph = StateGraph(dict)
 
 graph.add_node("planner", planner_agent)
+graph.add_node("architect", architect_agent)
+graph.add_edge("planner", "architect")
 graph.set_entry_point("planner")
 
 agent = graph.compile()
